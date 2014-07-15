64338bdf45a9b725bfab2e834809fbc3af509881
David Rojas <daxx4d@gmail.com>
2014-07-15 14:55:05 -0430

    algo

---
+++ main.js
@@ -0,0 +1,104 @@
+window.onload = main;
+
+var canvas,
+    ctx;
+
+function main(){
+    
+    canvas = document.getElementById("canvas");
+    ctx = canvas.getContext("2d");
+    
+    //run();
+    repaint();
+    
+}
+
+function run(){
+    
+    setTimeout(run,50);
+    act();
+    paint(ctx);
+    
+}
+
+function repaint(){
+    
+    //requestAnimationFrame(repaint);
+    paint(ctx);
+    
+}
+
+
+function act(){
+    
+    
+    
+}// act
+
+function paint(ctx){
+    
+    var width = canvas.width,
+        height = canvas.height,
+        anch = 20,
+        x = 0, y = 0,
+        posX = 0, posY = 0,
+        cont = 0;
+    
+    //dibuja el terreno
+    ctx.fillStyle = "#ba7f0b";
+    ctx.fillRect(0, 0, width-(anch*3), height)
+    
+    //se traza una cuadricula en el mapa
+    var cont = 0;
+    
+    for(posY = 0; posY < height;){
+    
+        for(posX = 0; posX < width-(anch*3);){
+        
+            ctx.moveTo(posX,      posY);
+            ctx.lineTo(posX+anch, posY);
+            ctx.lineTo(posX+anch, posY+anch);
+            ctx.lineTo(posX,      posY+anch);
+            ctx.lineTo(posX,      posY);
+            cont++;
+            
+            if(cont > 1000){ break; }
+            
+            posX += anch;
+        
+        }
+        
+        posY += anch;
+        posX = 0;
+        
+        if(cont > 1000){ break; }
+    
+    }
+    ctx.strokeStyle = "#000";
+    ctx.stroke();
+    
+}// paint
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ obj.js
@@ -0,0 +1,148 @@
+
+
+function Node(x, y){
+
+    this.pos = [ (x === null)? 0 : x, (y === null)? 0 : y ];
+    this.image = null;
+    this.Zone = null;
+    
+    this.zone = null;
+    
+    this.init = function(type){
+    
+        var path;
+        switch(type){
+        
+            case "res":
+                this.zone = new Res();
+                break;
+                
+            case "com":
+                this.zone = new Com();
+                break;
+                
+            case "ind":
+                this.zone = new Ind();
+                break;
+                
+            case "road":
+                this.zone = new Road();
+                path = "Borradores/simple_Road.jpg";
+                break;
+                
+            case "policeSt":
+                this.zone = new PoliceSt();
+                break;
+                
+            case "electFact":
+                this.zone = new electFact();
+                break;
+                
+            default:
+                console.error("valor no encontrado : "+type);
+                console.warn(this);
+                break;
+        
+        }
+        
+    
+    
+    }
+    
+    this.loadImage = function(path){
+    
+        var img = new Image();
+        img.src = path;
+    
+    }
+    
+    
+
+}
+    
+function Zone(){
+
+    this.type = null;
+    
+    this.elect = false;
+    this.cont = 0;
+    this.landValue = 0;
+    this.crime = 0;
+
+}  
+
+
+function Res(){
+
+    this.pop = 0;
+
+}
+
+Res.prototype = Object.create(Zone.prototype);
+
+function Com(){
+
+    this.jobs = 0;
+
+}
+Com.prototype = Object.create(Zone.prototype);
+    
+function Ind(){
+
+    this.jobs = 0;
+
+}
+Ind.prototype = Object.create(Zone.prototype);
+
+function electFact(){
+
+    this.maxMwh = 0;
+    this.usage = 0;
+
+}
+electFact.prototype = Object.create(Zone.prototype);
+
+function PoliceSt(){
+
+    this.funds = 0;
+
+}
+PoliceSt.prototype = Object.create(Zone.prototype);
+
+function Road(){
+
+    this.traffic = 0;
+    this.maxUsage = 1000;
+    this.usage = 0;
+
+}
+Road.prototype = Object.create(Zone.prototype);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
